56e1a50e1120bbf90d898dd0e3264384296cf40e
ersepobservatorio-cyt <ersep.observatorio@gmail.com>
2026-08-19 13:05:11 -0300

    Update permisos-config.js

--- assets/js/permisos-config.js
+++ assets/js/permisos-config.js
@@ -1,12 +1,16 @@
 window.ERSEP_PERMISOS_CONFIG = Object.freeze({
-  API_BASE_URL: "https://portal-observatorio-ersep-permisos.onrender.com",
+  API_BASE_URL:
+    "https://portal-observatorio-ersep-permisos.onrender.com",
 
-  GOOGLE_OAUTH_CLIENT_ID:
+  GOOGLE_CLIENT_ID:
     "253893593941-ben23304bbehfadu5j8cfarf0996s50q.apps.googleusercontent.com",
 
-  HEALTH_PATH: "/api/health",
+  HEALTH_PATH:
+    "/api/health",
 
-  HEALTH_TIMEOUT_MS: 75000,
+  HEALTH_TIMEOUT_MS:
+    75000,
 
-  RETRY_DELAY_MS: 5000
+  RETRY_DELAY_MS:
+    5000
 });
