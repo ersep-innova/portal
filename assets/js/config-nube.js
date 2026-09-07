bae907cf7436494e5f25a883c14597ad5380eedc
ersep-innova <ersep.observatorio@gmail.com>
2026-09-07 08:49:15 -0300

    Update config-nube.js

--- assets/js/config-nube.js
+++ assets/js/config-nube.js
@@ -9,6 +9,6 @@
      token → la misma clave que figura en la constante TOKEN del script
    ============================================================================= */
 window.CONFIG_NUBE = {
-  url: '',
-  token: ''
+  url: 'https://script.google.com/macros/s/AKfycbypZcqKOtfgs4FB_ElNb7gqcsQy-ccPmpL7G_0JI4jmislf030ZBokcmtdjx7BiV9Z3kw/exec',
+  token: 'AKfycbypZcqKOtfgs4FB_ElNb7gqcsQy-ccPmpL7G_0JI4jmislf030ZBokcmtdjx7BiV9Z3kw'
 };
