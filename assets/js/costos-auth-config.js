87ca8a635dfa3ee1de32586550b838aaa0b971f1
ersep-innova <ersep.observatorio@gmail.com>
2026-09-08 09:07:23 -0300

    Create costos-auth-config.js

---
+++ assets/js/costos-auth-config.js
@@ -0,0 +1,7 @@
+window.ERSEP_COSTOS_AUTH_CONFIG = {
+  version: 1,
+  iterations: 250000,
+  salt: "+v/a9Kuvs5aV5yis23hCJg==",
+  verifierIv: "9P1PHAoSkWuoSe18",
+  verifierCiphertext: "z7aQ5VtRedIF6CM8D/jWGS63zwoz6uhh/t7+XDgd9m5/uRpmU3N92F1C5DQSKr6fFA+u8A=="
+};
