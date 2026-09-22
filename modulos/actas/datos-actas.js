b6bf87a67c57227a8c1e4a2c74f308a2893902d7
ersep-innova <ersep.observatorio@gmail.com>
2026-09-21 21:09:48 -0300

    Delete modulos/actas/datos-actas.js

--- modulos/actas/datos-actas.js
+++
@@ -1,4 +0,0 @@
-/* Actas por empresa · todavía sin publicar.
-   Este archivo se reemplaza por el que genera modulos/actas/generador.html
-   Contiene sólo datos CIFRADOS; nunca contraseñas. */
-window.ACTAS_CIFRADAS = {};
